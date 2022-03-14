59ec1e47bce1ac85b12a9d4a0594e492ae15e88b
Mark Wiseman <wiseman.m@hotmail.co.uk>
2022-03-14 14:46:44 +0000

    Add project files
    Cubeletics build 1.0.0 and associated Unity project files.

---
+++ Unity Project/Cubeletics/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Public shows in inspector
+    public Rigidbody rb;
+
+    // 'f' just specifies it is a float number. Best to always include for floats
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    // FixedUpdate is best for physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            // Force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            // Force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            // Main menu
+            SceneManager.LoadScene(0);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Unity Project/Cubeletics/Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    void Update()
+    {
+        if (player.position.z <= 0)
+        {
+            scoreText.text = "0";
+        }
+        else if (player.position.z >= 1000)
+        {
+            scoreText.text = "1000";
+        }
+        else
+        {
+            scoreText.text = player.position.z.ToString("0");
+        }
+    }
+}
